adfade519641ba60428e3243ea7bd5a5b28bb0d6
Christian Souza <soouzachristian@gmail.com>
2025-02-02 18:00:16 -0300

    feat(Swagger): Add Swagger documentation

--- src/settings.py
+++ src/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'api',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = { 
+    'SECURITY_DEFINITIONS' : { 
+        'Basic' : { 
+            'type' : 'basic'
+         } 
+    } 
+}
